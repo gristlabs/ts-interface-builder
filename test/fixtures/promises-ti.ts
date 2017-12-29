2e757fbd69048a4e6540a0bb6e8bba6dfb27b159
Dmitry S <dsagal+git@gmail.com>
2017-12-28 23:47:40 -0500

    Fix tests broken by recent changes

--- test/fixtures/promises-ti.ts
+++ test/fixtures/promises-ti.ts
@@ -1,8 +1,8 @@
 import * as t from "ts-interface-checker";
 
-export const ILRUCache = t.iface([], [
-  t.prop("capacity", "number"),
-  t.prop("isReady", "boolean"),
-  t.prop("set", t.func("boolean", t.param("item", "ICacheItem"), t.param("overwrite", "boolean", true))),
-  t.prop("get", t.func("ICacheItem", t.param("key", "string"))),
-]);
+export const ILRUCache = t.iface([], {
+  "capacity": "number",
+  "isReady": "boolean",
+  "set": t.func("boolean", t.param("item", "ICacheItem"), t.param("overwrite", "boolean", true)),
+  "get": t.func("ICacheItem", t.param("key", "string")),
+});
